d04bfdffc29f2e9913072344007c64a359346dc8
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-01 23:49:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from preprocessing import full_prep
+# from preprocessing import full_prep
+from full_prep import full_prep
 from config_submit import config as config_submit
 
 import torch
@@ -18,21 +19,19 @@ from test_detect import test_detect
 from importlib import import_module
 import pandas
 
-datapath = config_submit['datapath']
-prep_result_path = config_submit['preprocess_result_path']
-skip_prep = config_submit['skip_preprocessing']
-skip_detect = config_submit['skip_detect']
-
+datapath = config_submit['datapath']#'/home/zhaojie/zhaojie/Lung/data/2017DataScienceBowl/stage2/stage2/'
+prep_result_path = config_submit['preprocess_result_path']#'./prep_result/'
+skip_prep = config_submit['skip_preprocessing']#False
+skip_detect = config_submit['skip_detect']#False
+print('-----------------')
 if not skip_prep:
-    testsplit = full_prep(datapath,prep_result_path,
-                          n_worker = config_submit['n_worker_preprocessing'],
-                          use_existing=config_submit['use_exsiting_preprocessing'])
+    testsplit = full_prep(datapath,prep_result_path,n_worker =None,use_existing=True)
 else:
     testsplit = os.listdir(datapath)
 
-nodmodel = import_module(config_submit['detector_model'].split('.py')[0])
+nodmodel = import_module(config_submit['detector_model'].split('.py')[0])#'net_detector'
 config1, nod_net, loss, get_pbb = nodmodel.get_model()
-checkpoint = torch.load(config_submit['detector_param'])
+checkpoint = torch.load(config_submit['detector_param'])#'./model/detector.ckpt'
 nod_net.load_state_dict(checkpoint['state_dict'])
 
 torch.cuda.set_device(0)
@@ -47,23 +46,29 @@ if not os.path.exists(bbox_result_path):
 
 if not skip_detect:
     margin = 32
-    sidelen = 144
+    # sidelen = 144
+    sidelen = 128
     config1['datadir'] = prep_result_path
-    split_comber = SplitComb(sidelen,config1['max_stride'],config1['stride'],margin,pad_value= config1['pad_value'])
+    split_comber = SplitComb(sidelen,config1['max_stride'],config1['stride'],margin,pad_value= config1['pad_value'])#144,16,4,32,170
 
     dataset = DataBowl3Detector(testsplit,config1,phase='test',split_comber=split_comber)
     test_loader = DataLoader(dataset,batch_size = 1,
         shuffle = False,num_workers = 32,pin_memory=False,collate_fn =collate)
 
-    test_detect(test_loader, nod_net, get_pbb, bbox_result_path,config1,n_gpu=config_submit['n_gpu'])
-
+    # test_detect(test_loader, nod_net, get_pbb, bbox_result_path,config1,n_gpu=config_submit['n_gpu'])
+    iter1, iter2, iter3, iter4 = next(iter(test_loader))
+    print('len(test_loader)',len(test_loader))#506
+    print("iter1: ", len(iter1))#1
+    print("iter2: ", len(iter2))#1
+    print("iter3: ", len(iter3))
+    print("iter4: ", iter4)
     
 
 
-casemodel = import_module(config_submit['classifier_model'].split('.py')[0])
+casemodel = import_module(config_submit['classifier_model'].split('.py')[0])#'net_classifier'
 casenet = casemodel.CaseNet(topk=5)
 config2 = casemodel.config
-checkpoint = torch.load(config_submit['classifier_param'])
+checkpoint = torch.load(config_submit['classifier_param'])#'./model/classifier.ckpt'
 casenet.load_state_dict(checkpoint['state_dict'])
 
 torch.cuda.set_device(0)
@@ -71,7 +76,7 @@ casenet = casenet.cuda()
 cudnn.benchmark = True
 casenet = DataParallel(casenet)
 
-filename = config_submit['outputfile']
+filename = config_submit['outputfile']#'prediction.csv'
 
 
 
@@ -96,8 +101,8 @@ def test_casenet(model,testset):
         #print([i,data_loader.dataset.split[i,1],casePred.data.cpu().numpy()])
     predlist = np.concatenate(predlist)
     return predlist    
-config2['bboxpath'] = bbox_result_path
-config2['datadir'] = prep_result_path
+config2['bboxpath'] = bbox_result_path#'./bbox_result'
+config2['datadir'] = prep_result_path#'./prep_result/'
 
 
 
