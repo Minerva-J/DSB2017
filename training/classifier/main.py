8fc4c5ba376ffeeb30a541560439e04e24accc9c
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-03 22:50:18 +0800

    Update main.py

--- training/classifier/main.py
+++ training/classifier/main.py
@@ -37,7 +37,7 @@ parser.add_argument('-b', '--batch-size', default=16, type=int,
                     metavar='N', help='mini-batch size (default: 16)')
 parser.add_argument('-b2', '--batch-size2', default=3, type=int,
                     metavar='N', help='mini-batch size (default: 16)')
-parser.add_argument('--lr', '--learning-rate', default=None, type=float,
+parser.add_argument('--lr', '--learning-rate', default=1e-2, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
@@ -77,9 +77,9 @@ def main():
     ##################################
 
     nodmodel = import_module(args.model1)
-    config1, nod_net, loss, get_pbb = nodmodel.get_model()
-    args.lr_stage = config1['lr_stage']
-    args.lr_preset = config1['lr']
+    config1, nod_net, loss, get_pbb = nodmodel.get_model()#net_detector_3
+    args.lr_stage = config1['lr_stage']#np.array([50,100,140,160])
+    args.lr_preset = config1['lr']#[0.01,0.001,0.0001,0.00001]
 
     
     save_dir = args.save_dir
@@ -87,16 +87,16 @@ def main():
     
     ##################################
     
-    casemodel = import_module(args.model2)
+    casemodel = import_module(args.model2)#net_classifier_3
     
     config2 = casemodel.config
-    args.lr_stage2 = config2['lr_stage']
-    args.lr_preset2 = config2['lr']
-    topk = config2['topk']
+    args.lr_stage2 = config2['lr_stage']#np.array([50,100,140,160])
+    args.lr_preset2 = config2['lr']#[0.01,0.001,0.0001,0.00001]
+    topk = config2['topk']#5
     case_net = casemodel.CaseNet(topk = topk,nodulenet=nod_net)
 
-    args.miss_ratio = config2['miss_ratio']
-    args.miss_thresh = config2['miss_thresh']
+    args.miss_ratio = config2['miss_ratio']#1
+    args.miss_thresh = config2['miss_thresh']#0.03
     if args.debug:
         args.save_dir = 'debug'
     
@@ -108,8 +108,9 @@ def main():
     
     
     ################################
-    start_epoch = args.start_epoch
+    start_epoch = args.start_epoch#30
     if args.resume:
+        print('resume',args.resume)
         checkpoint = torch.load(args.resume)
         if start_epoch == 0:
             start_epoch = checkpoint['epoch'] + 1
@@ -127,7 +128,7 @@ def main():
         else:
             save_dir = os.path.join('results',save_dir)
     if args.epochs == None:
-        end_epoch = args.lr_stage2[-1]
+        end_epoch = args.lr_stage2[-1]#160
     else:
         end_epoch = args.epochs
     ################################
@@ -180,52 +181,67 @@ def main():
         df.columns={'id','cancer'}
         df.to_csv('stage2_ans.csv',index=False)
         return
-    print(save_dir)
-    print(args.save_freq)
+    print('save_dir',save_dir)
+    print('save_freq',args.save_freq)
+    print('----------Loading Data')
     trainsplit = np.load('kaggleluna_full.npy')
     valsplit = np.load('valsplit.npy')
     testsplit = np.load('test.npy')
-
+    ##############################################
     dataset = DataBowl3Detector(trainsplit,config1,phase = 'train')
     train_loader_nod = DataLoader(dataset,batch_size = args.batch_size,
-        shuffle = True,num_workers = args.workers,pin_memory=True)
+        shuffle = True,num_workers = args.workers,pin_memory=True)#kaggleluna_full
 
     dataset = DataBowl3Detector(valsplit,config1,phase = 'val')
     val_loader_nod = DataLoader(dataset,batch_size = args.batch_size,
-        shuffle = False,num_workers = args.workers,pin_memory=True)
-
-    optimizer = torch.optim.SGD(nod_net.parameters(),
-        args.lr,momentum = 0.9,weight_decay = args.weight_decay)
-    
+        shuffle = False,num_workers = args.workers,pin_memory=True)#valsplit
+    iter1, iter2, iter3 = next(iter(train_loader_nod))
+    # print('len(train_loader_nod)',len(train_loader_nod))#1881
+    # print('len(val_loader_nod)',len(val_loader_nod))#216
+    # print("iter1: ", iter1.size())#([1, 1, 128, 128, 128])
+    # print("iter2: ", iter2.size())#([1, 32, 32, 32, 3, 5])
+    # print("iter3: ", iter3.size())#([1, 3, 32, 32, 32])
+    optimizer = torch.optim.SGD(nod_net.parameters(), args.lr,momentum = 0.9,weight_decay = args.weight_decay)
+    #########################################
     trainsplit = np.load('full.npy')
     dataset = DataBowl3Classifier(trainsplit,config2,phase = 'train')
     train_loader_case = DataLoader(dataset,batch_size = args.batch_size2,
-        shuffle = True,num_workers = args.workers,pin_memory=True)
+        shuffle = True,num_workers = args.workers,pin_memory=True)#full
     
     dataset = DataBowl3Classifier(valsplit,config2,phase = 'val')
     val_loader_case = DataLoader(dataset,batch_size = max([args.batch_size2,1]),
-        shuffle = False,num_workers = args.workers,pin_memory=True)
+        shuffle = False,num_workers = args.workers,pin_memory=True)#valsplit
 
     dataset = DataBowl3Classifier(trainsplit,config2,phase = 'val')
     all_loader_case = DataLoader(dataset,batch_size = max([args.batch_size2,1]),
-        shuffle = False,num_workers = args.workers,pin_memory=True)
-
+        shuffle = False,num_workers = args.workers,pin_memory=True)#full
+    iter1, iter2, iter3, iter4 = next(iter(train_loader_case))
+    # print('len(train_loader_case)',len(train_loader_case))#1595
+    # print('len(val_loader_case)',len(val_loader_case))#350
+    # print("iter1: ", iter1.size())#([1, 5, 1, 96, 96, 96])
+    # print("iter2: ", iter2.size())#([1, 5, 3, 24, 24, 24])
+    # print("iter3: ", iter3.size())#([1, 5])isnodlist
+    # print("iter4: ", iter4.size())#([1, 1, 1])
     optimizer2 = torch.optim.SGD(case_net.parameters(),
         args.lr,momentum = 0.9,weight_decay = args.weight_decay)
-    
+    ###############################################
     for epoch in range(start_epoch, end_epoch + 1):
-        if epoch ==start_epoch:
-            lr = args.lr
+        if epoch ==start_epoch:#30
+            print('Epoch-train_casenet',epoch)
+            lr = args.lr#1e-2
             debug = args.debug
             args.lr = 0.0
             args.debug = True
             train_casenet(epoch,case_net,train_loader_case,optimizer2,args)
             args.lr = lr
             args.debug = debug
-        if epoch<args.lr_stage[-1]:
+            # print(stop)
+        if epoch<args.lr_stage[-1]:#[50,100,140,160]160
+            print('Epoch-train_nodulenet',epoch)
             train_nodulenet(train_loader_nod, nod_net, loss, epoch, optimizer, args)
             validate_nodulenet(val_loader_nod, nod_net, loss)
-        if epoch>config2['startepoch']:
+        if epoch>config2['startepoch']:#20
+            print('Epoch-train_casenet',epoch)
             train_casenet(epoch,case_net,train_loader_case,optimizer2,args)
             val_casenet(epoch,case_net,val_loader_case,args)
             val_casenet(epoch,case_net,all_loader_case,args)
@@ -235,12 +251,12 @@ def main():
             for key in state_dict.keys():
                 state_dict[key] = state_dict[key].cpu()
 
-            torch.save({
-                'epoch': epoch,
-                'save_dir': save_dir,
-                'state_dict': state_dict,
-                'args': args},
-                os.path.join(save_dir, '%03d.ckpt' % epoch))
+            # torch.save({
+                # 'epoch': epoch,
+                # 'save_dir': save_dir,
+                # 'state_dict': state_dict,
+                # 'args': args},
+                # os.path.join(save_dir, '%03d.ckpt' % epoch))
 if __name__ == '__main__':
     main()
 
