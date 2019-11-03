34c0702756ea83fa7b3a257418cf93212c88c3c9
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-03 23:01:18 +0800

    Update trainval_classifier.py

--- training/classifier/trainval_classifier.py
+++ training/classifier/trainval_classifier.py
@@ -9,6 +9,7 @@ from torch import nn
 from torch import optim
 from torch.autograd import Variable
 from torch.nn.functional import cross_entropy,sigmoid,binary_cross_entropy
+from torch.nn import BCELoss
 from torch.utils.data import DataLoader
 
 def get_lr(epoch,args):
@@ -55,25 +56,31 @@ def train_casenet(epoch,model,data_loader,optimizer,args):
 #         weight = 3*torch.ones(y.size()).float().cuda()
         optimizer.zero_grad()
         nodulePred,casePred,casePred_each = model(x,coord)
+        # print('0',nodulePred.shape,casePred.shape,casePred_each.shape)#([4, 5, 207360]) torch.Size([4]) torch.Size([4, 5])
+        casePred = casePred.unsqueeze(1)
+        # print('casePred',casePred,y[:,0])#tensor([[0.0586],[0.3742],[0.0571],[0.4312]], device='cuda:0', grad_fn=<UnsqueezeBackward0>) tensor([[0.],[1.],[0.],[0.]], device='cuda:0')
         loss2 = binary_cross_entropy(casePred,y[:,0])
-        missMask = (casePred_each<args.miss_thresh).float()
+        # loss2 = BCELoss(casePred,y[:,0])
+        missMask = (casePred_each<args.miss_thresh).float()#0.03
         missLoss = -torch.sum(missMask*isnod*torch.log(casePred_each+0.001))/xsize[0]/xsize[1]
         loss = loss2+args.miss_ratio*missLoss
         loss.backward()
         #torch.nn.utils.clip_grad_norm(model.parameters(), 1)
-
+        # print('shape',casePred.shape, ydata.shape)
         optimizer.step()
-        loss2Hist.append(loss2.data[0])
-        missHist.append(missLoss.data[0])
+        loss2Hist.append(loss2.item())
+        missHist.append(missLoss.item())
         lenHist.append(len(x))
         outdata = casePred.data.cpu().numpy()
 
-        pred = outdata>0.5
-        tpn += np.sum(1==pred[ydata==1])
-        fpn += np.sum(1==pred[ydata==0])
-        fnn += np.sum(0==pred[ydata==1])
-        acc = np.mean(ydata==pred)
+        outdata = outdata>0.5
+        # print('0',outdata.shape,len([ydata==0]))
+        # print('1',outdata.shape,ydata.shape)
+        fpn += np.sum(1==outdata[ydata==0])
+        fnn += np.sum(0==outdata[ydata==1])
+        acc = np.mean(ydata==outdata)
         accHist.append(acc)
+        del x, coord, isnod, y, ydata, nodulePred,casePred,casePred_each, missMask, outdata
         
     endtime = time.time()
     lenHist = np.array(lenHist)
@@ -102,22 +109,22 @@ def val_casenet(epoch,model,data_loader,args):
 
     for i,(x,coord,isnod,y) in enumerate(data_loader):
 
-        coord = Variable(coord,volatile=True).cuda()
-        x = Variable(x,volatile=True).cuda()
+        coord = Variable(coord).cuda()
+        x = Variable(x).cuda()
         xsize = x.size()
         ydata = y.numpy()[:,0]
         y = Variable(y).float().cuda()
         isnod = Variable(isnod).float().cuda()
 
         nodulePred,casePred,casePred_each = model(x,coord)
-        
+        casePred = casePred.unsqueeze(1)
         loss2 = binary_cross_entropy(casePred,y[:,0])
         missMask = (casePred_each<args.miss_thresh).float()
         missLoss = -torch.sum(missMask*isnod*torch.log(casePred_each+0.001))/xsize[0]/xsize[1]
 
         #loss2 = binary_cross_entropy(sigmoid(casePred),y[:,0])
-        loss2Hist.append(loss2.data[0])
-        missHist.append(missLoss.data[0])
+        loss2Hist.append(loss2.item())
+        missHist.append(missLoss.item())
         lenHist.append(len(x))
         outdata = casePred.data.cpu().numpy()
         #print([i,data_loader.dataset.split[i,1],sigmoid(casePred).data.cpu().numpy()])
