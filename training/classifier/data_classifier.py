51a079b843ac56bff9e1a3f0d59fb77ccb31484d
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-03 22:44:12 +0800

    Update data_classifier.py

--- training/classifier/data_classifier.py
+++ training/classifier/data_classifier.py
@@ -16,32 +16,38 @@ class DataBowl3Classifier(Dataset):
     def __init__(self, split, config, phase = 'train'):
         assert(phase == 'train' or phase == 'val' or phase == 'test')
         
-        self.random_sample = config['random_sample']
-        self.T = config['T']
-        self.topk = config['topk']
-        self.crop_size = config['crop_size']
-        self.stride = config['stride']
-        self.augtype  = config['augtype']
+        self.random_sample = config['random_sample']#True
+        self.T = config['T']#1
+        self.topk = config['topk']#5
+        self.crop_size = config['crop_size']#[96,96,96]
+        self.stride = config['stride']#4
+        self.augtype  = config['augtype']#{'flip':True,'swap':False,'rotate':False,'scale':False}
         #self.labels = np.array(pandas.read_csv(config['labelfile']))
         
-        datadir = config['datadir']
-        bboxpath  = config['bboxpath']
+        datadir = config['datadir']#preprocess_result_path
+        bboxpath  = config['bboxpath']#'../detector/results/res18/bbox/',
         self.phase = phase
         self.candidate_box = []
         self.pbb_label = []
         
+        split = [str(f,'utf-8') for f in split]
         idcs = split
         self.filenames = [os.path.join(datadir, '%s_clean.npy' % idx) for idx in idcs]
-        labels = np.array(pandas.read_csv(config['labelfile']))
+        labels = np.array(pandas.read_csv(config['labelfile']))#'./full_label.csv'
         if phase !='test':
+            # print('0',labels[:,0])
+            # print('0=', [str(f,'utf-8') for f in split])
+            # self.yset = np.array([labels[labels[:,0]==str(f,'utf-8'),1] for f in split]).astype('int')
             self.yset = np.array([labels[labels[:,0]==f.split('-')[0].split('_')[0],1] for f in split]).astype('int')
-        idcs = [f.split('-')[0] for f in idcs]
+        # print([str(f,'utf-8') for f in idcs])
+        # idcs = [str(f,'utf-8') for f in idcs]
         
         
         for idx in idcs:
             pbb = np.load(os.path.join(bboxpath,idx+'_pbb.npy'))
-            pbb = pbb[pbb[:,0]>config['conf_th']]
-            pbb = nms(pbb, config['nms_th'])
+            # print('pbb.shape',pbb.shape)#(~,5)
+            pbb = pbb[pbb[:,0]>config['conf_th']]#-1
+            pbb = nms(pbb, config['nms_th'])#0.05
             
             lbb = np.load(os.path.join(bboxpath,idx+'_lbb.npy'))
             pbb_label = []
@@ -50,7 +56,7 @@ class DataBowl3Classifier(Dataset):
                 isnod = False
                 for l in lbb:
                     score = iou(p[1:5], l)
-                    if score > config['detect_th']:
+                    if score > config['detect_th']:#0.05
                         isnod = True
                         break
                 pbb_label.append(isnod)
@@ -63,20 +69,22 @@ class DataBowl3Classifier(Dataset):
     def __getitem__(self, idx,split=None):
         t = time.time()
         np.random.seed(int(str(t%1)[2:7]))#seed according to time
-
+        # print('idx',idx)
         pbb = self.candidate_box[idx]
         pbb_label = self.pbb_label[idx]
         conf_list = pbb[:,0]
         T = self.T
         topk = self.topk
         img = np.load(self.filenames[idx])
+        # print('-',img.shape,self.filenames[idx])#(1, 320, 206, 322) ../Data/preprocess/d92998a73d4654a442e6d6ba15bbb827_clean.npy
         if self.random_sample and self.phase=='train':
             chosenid = sample(conf_list,topk,T=T)
             #chosenid = conf_list.argsort()[::-1][:topk]
         else:
             chosenid = conf_list.argsort()[::-1][:topk]
         croplist = np.zeros([topk,1,self.crop_size[0],self.crop_size[1],self.crop_size[2]]).astype('float32')
-        coordlist = np.zeros([topk,3,self.crop_size[0]/self.stride,self.crop_size[1]/self.stride,self.crop_size[2]/self.stride]).astype('float32')
+        # print(self.crop_size[0], self.stride, self.crop_size[1], self.stride, self.crop_size[2], self.stride)
+        coordlist = np.zeros([topk,3,self.crop_size[0]//self.stride,self.crop_size[1]//self.stride,self.crop_size[2]//self.stride]).astype('float32')
         padmask = np.concatenate([np.ones(len(chosenid)),np.zeros(self.topk-len(chosenid))])
         isnodlist = np.zeros([topk])
 
@@ -85,6 +93,7 @@ class DataBowl3Classifier(Dataset):
             target = pbb[id,1:]
             isnod = pbb_label[id]
             crop,coord = self.crop(img,target)
+            # print('-',crop.shape,coord.shape)#(1, 96, 96, 96) (3, 24, 24, 24)
             if self.phase=='train':
                 crop,coord = augment(crop,coord,
                                  ifflip=self.augtype['flip'],ifrotate=self.augtype['rotate'],
@@ -96,6 +105,8 @@ class DataBowl3Classifier(Dataset):
             
         if self.phase!='test':
             y = np.array([self.yset[idx]])
+            # print('-',isnodlist, y)#[1. 0. 0. 0. 0.] [[1]]
+            # print('-',croplist.shape, torch.from_numpy(coordlist).shape, torch.from_numpy(isnodlist).shape, torch.from_numpy(y).shape)#(5, 1, 96, 96, 96) torch.Size([5, 3, 24, 24, 24]) torch.Size([5]) torch.Size([1, 1])
             return torch.from_numpy(croplist).float(), torch.from_numpy(coordlist).float(), torch.from_numpy(isnodlist).int(), torch.from_numpy(y)
         else:
             return torch.from_numpy(croplist).float(), torch.from_numpy(coordlist).float(), torch.from_numpy(isnodlist).int()
@@ -109,19 +120,19 @@ class DataBowl3Classifier(Dataset):
         
 class simpleCrop():
     def __init__(self,config,phase):
-        self.crop_size = config['crop_size']
-        self.scaleLim = config['scaleLim']
-        self.radiusLim = config['radiusLim']
-        self.jitter_range = config['jitter_range']
-        self.isScale = config['augtype']['scale'] and phase=='train'
-        self.stride = config['stride']
-        self.filling_value = config['filling_value']
+        self.crop_size = config['crop_size']#[96,96,96]
+        self.scaleLim = config['scaleLim']#[0.85,1.15]
+        self.radiusLim = config['radiusLim']#[6,100]
+        self.jitter_range = config['jitter_range']#0.15
+        self.isScale = config['augtype']['scale'] and phase=='train'#False
+        self.stride = config['stride']#4
+        self.filling_value = config['filling_value']#160
         self.phase = phase
         
     def __call__(self,imgs,target):
         if self.isScale:
-            radiusLim = self.radiusLim
-            scaleLim = self.scaleLim
+            radiusLim = self.radiusLim#[6,100]
+            scaleLim = self.scaleLim#[0.85,1.15]
             scaleRange = [np.min([np.max([(radiusLim[0]/target[3]),scaleLim[0]]),1])
                          ,np.max([np.min([(radiusLim[1]/target[3]),scaleLim[1]]),1])]
             scale = np.random.rand()*(scaleRange[1]-scaleRange[0])+scaleRange[0]
@@ -167,15 +178,17 @@ class simpleCrop():
                 pad2 = [[0,0],[0,newpad],[0,newpad],[0,newpad]]
                 crop = np.pad(crop,pad2,'constant',constant_values =self.filling_value)
 
-        return crop,coord
+        return crop,coord#(1, 96, 96, 96) (3, 24, 24, 24)
 
 def sample(conf,N,T=1):
     if len(conf)>N:
-        target = range(len(conf))
+        # print('len(conf)',len(conf))
+        target = [i for i in range(len(conf))]
         chosen_list = []
         for i in range(N):
             chosenidx = sampleone(target,conf,T)
             chosen_list.append(target[chosenidx])
+            # print('target',target, chosenidx)
             target.pop(chosenidx)
             conf = np.delete(conf, chosenidx)
 
@@ -189,6 +202,7 @@ def sampleone(target,conf,T):
     p = softmax(conf/T)
     p = np.max([np.ones_like(p)*0.00001,p],axis=0)
     p = p/np.sum(p)
+    # print(len(target),p)
     return np.random.choice(np.arange(len(target)),size=1,replace = False, p=p)[0]
 
 def softmax(x):
